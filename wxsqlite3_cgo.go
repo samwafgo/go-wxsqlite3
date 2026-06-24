557b5c4c552a93a910bf9334b81812aa1aa0a675
pc <tela2020@foxmail.com>
2026-06-24 17:01:50 +0800

    fix:arch

--- wxsqlite3_cgo.go
+++ wxsqlite3_cgo.go
@@ -1,8 +1,9 @@
 package sqlite3
 
 /*
-#cgo amd64 CFLAGS: -msse4.1
-#cgo !arm64,!loong64,!loongarch64 CFLAGS: -maes
+// AES-NI 硬件加速路径（aes_hardware.c）仅在 x86（amd64/386）下编译，
+// 需要同时启用 SSE4.1 与 AES 指令集；其它架构走软件实现，不要传这些 x86 专有选项。
+#cgo amd64 386 CFLAGS: -msse4.1 -maes
 #cgo CFLAGS: -DUSE_LIBSQLITE3 -DCODEC_TYPE=CODEC_TYPE_AES128 -DSQLITE_HAS_CODEC=1
 */
 import "C"
